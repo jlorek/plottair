b0e9e4344d53bb0514e85afca702b84d61b98765
jlorek <lorek.jens@gmail.com>
2018-05-28 14:44:43 +0200

    some vuejs sprinkles

--- ui/lib/ui_web/channels/room_channel.ex
+++ ui/lib/ui_web/channels/room_channel.ex
@@ -4,7 +4,7 @@ defmodule UiWeb.RoomChannel do
   def join("room:lobby", _message, socket) do
     {:ok, socket}
   end
-  
+
   def join("room:" <> _private_room_id, _params, _socket) do
     {:error, %{reason: "unauthorized"}}
   end
